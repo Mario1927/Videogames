eea9af4dfa063bd891f16b2e8d92a4e2fd73c007
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-02-27 10:19:40 -0600

    Filters component, and style updates

---
+++ client/src/components/Filters/Filters.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { FiltersWrapper, FilterSelects, FiltersButton, FilterWrapper, FilterLogo } from "../Styled/Filters";
+import FilterPNG from "../images/Filter.png";
+
+export default function Filters({handlerFilterByGenre, handlerFilterByCreated, handlerSortByName, handlerSortByRating, genres}) {
+
+    function onClick(event){
+        event.preventDefault();
+        document.querySelector(`.Logo`).classList.toggle('active')
+        document.querySelector(`.Filters`).classList.toggle('active')
+    }
+
+    return (
+        <React.Fragment>
+            <FilterLogo src={FilterPNG} alt="Filter" className="Logo" onClick={onClick}/>
+            <FiltersWrapper className="Filters">
+                <FilterWrapper>
+                    <FilterSelects className="Genre" onChange={(event) => handlerFilterByGenre(event)}>
+                    <option value={'All'}>Genres</option>
+                    {genres.map(genre => {
+                        return <option key={genre.id} value={genre.name}>{genre.name}</option>
+                    })}
+                    </FilterSelects>
+                    <FilterSelects className="Created" onChange={(event) => handlerFilterByCreated(event)}>
+                        <option value='All'>Origin</option>
+                        <option value={true}>Created</option>
+                        <option value={false}>Existing</option>
+                    </FilterSelects>
+                </FilterWrapper>
+                <FilterWrapper>
+                    <FilterSelects className="SortByName" onChange={(event) => handlerSortByName(event)}>
+                        <option value='None'>Alphabetical</option>
+                        <option value='ASC'>A-Z</option>
+                        <option value='DESC'>Z-A</option>
+                    </FilterSelects>
+                    <FilterSelects className="SortByRating" onChange={(event) => handlerSortByRating(event)}>
+                        <option value='None'>Rating</option>
+                        <option value='ASC'>Ascendent</option>
+                        <option value='DESC'>Descendent</option>
+                    </FilterSelects>
+                </FilterWrapper>
+                
+            </FiltersWrapper>
+        </React.Fragment>
+    )
+}
---
+++ client/src/components/Styled/Filters.js
@@ -0,0 +1,51 @@
+import styled from "styled-components";
+
+export const FiltersWrapper = styled.div`
+    display: none;
+    margin-top: 10px;
+    justify-content: right;
+    align-items: center;
+    &.active {
+        display: flex;
+    }
+`
+
+export const FilterSelects = styled.select`
+    margin-left: 2.5px;
+    margin-right: 2.5px;
+    font-family: "Cascadia Code",Consolas,Monaco,"Andale Mono","Ubuntu Mono",monospace;
+    font-size: 15px;
+    background-color: rgba(0, 0, 0, 0.85);
+    color: white;
+    border-color: #00D1FF;
+`
+
+export const FilterWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+`
+
+export const FiltersButton = styled.button`
+    background: none;
+    border: none;
+    color: #00D1FF;
+`
+
+export const FilterLogo = styled.img`
+    display: block;
+    width: 75px;
+    height: 75px;
+    margin-top: 5px;
+    margin-left: auto;
+    margin-right: auto;
+    align-self: flex-end;
+
+    &:hover {
+        cursor: pointer;
+        border: 2px solid #00D1FF;
+    }
+
+    &.active {
+        border: 2px solid #00D1FF;
+    }
+`
