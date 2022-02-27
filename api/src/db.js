59215eb6fbbaf32ad50d57f6e2ab14b9f1ecb9ec
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-02-27 17:00:51 -0600

    Api updates

--- api/src/db.js
+++ api/src/db.js
@@ -31,7 +31,7 @@ let sequelize =
         ssl: true,
       })
     : new Sequelize(
-        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
+        `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
         { logging: false, native: false }
       );
 
