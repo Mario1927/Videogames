f85287d288c2ec6b0fd9a9df369477caef5deffe
Mario1927 <hernandez.mario@correounivalle.edu.co>
2022-03-04 01:01:00 -0600

    Minor update

--- client/src/components/Filters/Filters.jsx
+++ client/src/components/Filters/Filters.jsx
@@ -9,56 +9,50 @@ export default function Filters() {
     const dispatch = useDispatch();
     const genres = useSelector(state => state.genres);
 
-    function onClick(event){
-        event.preventDefault();
-        document.querySelector(`.Logo`).classList.toggle('active')
-        document.querySelector(`.Filters`).classList.toggle('active')
-    }
-
-    function handlerSortByName(event) {
-        dispatch(sortGamesByName(event.target.value))
+    function handlerSortByName(value) {
+        dispatch(sortGamesByName(value))
         document.querySelector('.SortByRating').selectedIndex = '0';
     }
 
-    function handlerSortByRating(event) {
-        dispatch(sortGamesByRating(event.target.value))
+    function handlerSortByRating(value) {
+        dispatch(sortGamesByRating(value))
         document.querySelector('.SortByName').selectedIndex = '0';
     }
 
-    function handlerFilterByGenre(event) {
-        dispatch(filterGamesByGenre(event.target.value))
+    function handlerFilterByGenre(value) {
+        dispatch(filterGamesByGenre(value))
         document.querySelector('.SortByName').selectedIndex = '0';
         document.querySelector('.SortByRating').selectedIndex = '0';
     }
 
-    function handlerFilterByCreated(event) {
-        dispatch(filterGamesByCreator(event.target.value))
+    function handlerFilterByCreated(value) {
+        dispatch(filterGamesByCreator(value))
         document.querySelector('.SortByName').selectedIndex = '0';
         document.querySelector('.SortByRating').selectedIndex = '0';
     }
 
     return (
         <React.Fragment>
-            <FilterLogo src={FilterPNG} alt="Filter" className="Logo active" onClick={onClick}/>
+            <FilterLogo src={FilterPNG} alt="Filter" className="Logo active"/>
             <FiltersWrapper className="Filters active">
                 <FilterWrapper>
-                    <FilterSelects className="Genre" onChange={(event) => handlerFilterByGenre(event)}>
+                    <FilterSelects className="Genre" onChange={(event) => handlerFilterByGenre(event.target.value)}>
                     <option value={'All'}>Genres</option>
                     {genres.map(genre => {
                         return <option key={genre.id} value={genre.name}>{genre.name}</option>
                     })}
                     </FilterSelects>
-                    <FilterSelects className="Created" onChange={(event) => handlerFilterByCreated(event)}>
+                    <FilterSelects className="Created" onChange={(event) => handlerFilterByCreated(event.target.value)}>
                         <option value='All'>Creator</option>
                         <option value={true}>Created</option>
                         <option value={false}>Existing</option>
                     </FilterSelects>
-                    <FilterSelects className="SortByName" onChange={(event) => handlerSortByName(event)}>
+                    <FilterSelects className="SortByName" onChange={(event) => handlerSortByName(event.target.value)}>
                         <option value='None'>Alphabetical</option>
                         <option value='ASC'>A-Z</option>
                         <option value='DESC'>Z-A</option>
                     </FilterSelects>
-                    <FilterSelects className="SortByRating" onChange={(event) => handlerSortByRating(event)}>
+                    <FilterSelects className="SortByRating" onChange={(event) => handlerSortByRating(event.target.value)}>
                         <option value='None'>Rating</option>
                         <option value='DESC'>Highest</option>
                         <option value='ASC'>Lowest</option>
